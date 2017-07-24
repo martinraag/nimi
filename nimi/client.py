00baf617a16b27f975d48e738cb8859881bd0f31
Martin Raag <martin.raag@gmail.com>
2017-07-25 01:50:06 +0300

    Initial client side CLI.

---
+++ nimi/client.py
@@ -0,0 +1,39 @@
+import hashlib
+import hmac
+import json
+
+import click
+import requests
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('url')
+@click.argument('hostname')
+@click.argument('secret')
+@click.option('--rate', default=5, help='Ping rate in minutes')
+def setup(url, hostname, secret, rate):
+    """Setup automatic execution of ping command"""
+
+    pass
+
+@cli.command()
+@click.argument('url')
+@click.argument('hostname')
+@click.argument('secret')
+def ping(url, hostname, secret):
+    """Call the Lambda function to update the IP of the hostname"""
+
+    payload = {
+        'hostname': hostname,
+        'signature': hmac.new(secret.encode('utf-8'), hostname.encode('utf-8'), hashlib.sha256)
+    }
+    requests.put(url, json.dumps(payload))
+
+
+if __name__ == '__main__':
+    cli()
