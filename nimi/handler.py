883d4486be7a3db60602599b9d67b75820c4e794
Martin Raag <martin.raag@gmail.com>
2017-07-25 13:46:10 +0300

    Refactor Lambda handler
    - Support multiple domain configurations
    - Single resource to update ip
    - Validate request signature

--- nimi/handler.py
+++ nimi/handler.py
@@ -1,22 +1,51 @@
+import hashlib
+import hmac
 import json
 import os
 
 import boto3
 
-DOMAIN_NAME = os.environ['DOMAIN_NAME']
-HOSTED_ZONE_ID = os.environ['HOSTED_ZONE_ID']
 
+CONFIG_OPTIONS = ['HOSTED_ZONE_ID', 'SHARED_SECRET']
 route53 = boto3.client('route53')
 
+
 def lambda_handler(event, context):
-    if event['httpMethod'] == 'GET':
-        return reflect_address(event, context)
-    elif event['httpMethod'] == 'PUT':
-        return update_address(event, context)
-    else:
-        return {
-            'statusCode': 404,
-        }
+    try:
+        request = json.loads(event['body'])
+    except json.JSONDecodeError:
+        return Response.bad_request('Invalid payload')
+
+    if not 'hostname' in request or not 'signature' in request:
+        return Response.bad_request('Invalid payload')
+
+    config = get_configuration(request['hostname'])
+    if not config:
+        return Response.bad_request('Invalid hostname')
+
+    signature = hmac.new(
+        config['shared_secret'].encode('utf-8'),
+        request['hostname'].encode('utf-8'),
+        hashlib.sha256
+    ).hexdigest()
+    if not hmac.compare_digest(signature, request['signature']):
+        return Response.unauthorized('Unauthorized')
+
+    current_ip = get_record(config['hosted_zone_id'], request['hostname'])
+    request_ip = event['requestContext']['identity']['sourceIp']
+    if not current_ip or not current_ip == request_ip:
+        set_record(config['hosted_zone_id'], request['hostname'], request_ip)
+
+    return Response.ok('Cool beans')
+
+
+def get_configuration(hostname):
+    env_prefix = hostname.replace('.', '_').upper()
+    options = set(['{}__{}'.format(env_prefix, option) for option in CONFIG_OPTIONS])
+    if not options.issubset(set(os.environ.keys())):
+        return
+    return {key.split('__')[1].lower(): value for key, value in os.environ.items() if key in options}
+
 
 def get_record(zone_id, record_name):
     record_sets = route53.list_resource_record_sets(
@@ -30,6 +59,7 @@ def get_record(zone_id, record_name):
     if len(records) > 1:
         raise Exception('Multiple records found for record {}'.format(record_name))
     return records[0]['Value'] if records else None
+
 
 def set_record(zone_id, record_name, ip_address):
     route53.change_resource_record_sets(
@@ -53,65 +83,26 @@ def set_record(zone_id, record_name, ip_address):
         }
     )
 
-def reflect_address(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps({
-            'address': event['requestContext']['identity']['sourceIp']
-        })
-    }
+
+class Response(object):
+    """Helper class to create Lambda proxy response"""
 
 
-def update_address(event, context):
-    if not DOMAIN_NAME or not HOSTED_ZONE_ID:
+    @classmethod
+    def ok(cls, message):
+        return cls.create(200, message)
+
+    @classmethod
+    def bad_request(cls, message):
+        return cls.create(400, message)
+
+    @classmethod
+    def unauthorized(cls, message):
+        return cls.create(401, message)
+
+    @classmethod
+    def create(cls, statusCode, message):
         return {
-            'statusCode': 500,
-            'body': json.dumps({
-                'error': 'Internal Server Error'
-            })
+            'statusCode': statusCode,
+            'body': json.dumps({'message': message})
         }
-
-    # if not event['headers']['Authorization'] or not event['headers']['Authorization'] == SHARED_SECRET:
-    #     return {
-    #         'statusCode': 401,
-    #         'body': json.dumps({
-    #             'error': 'Unauthorized'
-    #         })
-    #     }
-
-    try:
-        request = json.loads(event['body'])
-    except Exception:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({
-                'error': 'Invalid request body'
-            })
-        }
-    
-    if not request['domain'] == DOMAIN_NAME:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({
-                'error': 'Invalid domain'
-            })
-        }
-
-    try:
-        existing_ip = get_record(HOSTED_ZONE_ID, DOMAIN_NAME)
-    except Exception as err:
-        print(err)
-        return {
-            'statusCode': 500,
-            'body': json.dumps({
-                'error': 'Internal Server Error: {}'.format(err)
-            })
-        }
-
-    if not existing_ip or not existing_ip == request.address:
-        set_record(HOSTED_ZONE_ID, DOMAIN_NAME, request['address'])
-
-    return {
-        'statusCode': 200,
-        'body': event['body']
-    }
